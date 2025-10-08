53fde3d4147e55878088d9c3daef9d6ad555161c
chaedool <142567401+chaedool@users.noreply.github.com>
2025-10-08 14:22:57 +0900

    Add files via upload

---
+++ 02/dist_standard.py
@@ -0,0 +1,17 @@
+﻿def distance_standard(x, y):
+    """
+    Distance (standard)
+    
+    Args:
+        x (Point3D): First point
+        y (Point3D): Second point
+    
+    Retruns:
+        Distance between two input points (number)
+    """
+
+    import math
+
+    return math.sqrt((y.X - x.X)**2 + (y.Y - x.Y)**2 + (y.Z - x.Z)**2)
+
+a = distance_standard(x, y)
