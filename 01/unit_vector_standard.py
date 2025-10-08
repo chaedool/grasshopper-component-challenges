8ef7ab7100301d1db69043d5c9043dc30febcc1e
chaedool <142567401+chaedool@users.noreply.github.com>
2025-10-08 14:13:38 +0900

    Add files via upload

---
+++ 01/unit_vector_standard.py
@@ -0,0 +1,17 @@
+﻿def unit_vector_standard(x):
+    """
+    Distance (standard)
+    
+    Args:
+        x (Vector3d): Vector to unitize.
+    
+    Retruns:
+        Unitzed vector if x can be unitized, else x.
+    """
+
+    if x.IsValid and x.Length != 0:
+        x.Unitize()
+
+    return x
+
+a = unit_vector_standard(x)
---
+++ 01/unit_x_standard.py
@@ -0,0 +1,16 @@
+﻿def unit_x_standard(x):
+    """
+    Unit X (standard)
+    
+    Args:
+        x (float): Input value
+    
+    Retruns:
+        Vector X multiplied by x
+    """
+    
+    import Rhino.Geometry as rg
+    
+    return rg.Vector3d(x, 0, 0)
+
+a = unit_x_standard(x)
